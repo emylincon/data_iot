2fd4f515a01cd56cd0145a3fd7642121e1af1dc4
Emeka Ugwuanyi <emylincon@gmail.com>
2019-03-31 17:43:42 +0100

    initial commit

--- iot_server.py
+++ iot_server.py
@@ -9,6 +9,9 @@ import time
 import matplotlib.animation as animation
 
 
+cpu, mem, data = 0
+
+
 os.system('clear')
 print('-----------------------------------')
 print('Welcome to MQTT Publisher client')
@@ -25,6 +28,7 @@ print('-----------------------------------')
 client.username_pw_set(username, password)
 client.connect(broker_ip, broker_port_no, 60)
 
+'''
 while True:
     try:
         message = input('Input Message: ').strip().lower()
@@ -32,17 +36,21 @@ while True:
         print("Message Sent")
     except KeyboardInterrupt:
         print('\nProgramme Terminated')
+'''
+
 
 def get_cpu():
+    global cpu
     prev_t = 0
     next_t = psutil.cpu_percent(percpu=False)
     delta = abs(prev_t - next_t)
     prev_t = next_t
     # return delta     # Returns CPU util in percentage
-    cpu.append(delta)
+    cpu = delta
 
 
 def get_mem():
+    global mem
     cmd = ['cat /proc/meminfo | grep MemFree |cut -d ":" -f 2 | cut -d "k" -f 1']
     free_mem = str(sp.check_output(cmd, shell=True), 'utf-8')[0:-1]
     cmd = [' cat /proc/meminfo | grep MemAva |cut -d ":" -f 2 | cut -d "k" -f 1']
@@ -59,6 +67,25 @@ def get_storage():
     store.append(float(storage.strip()))
 
 
+def get_network(intf):
+    time_quanta = 3
+    if_name = intf  # list(psutil.net_if_stats().keys())[0]
+    # if if_name == 'lo':
+    # if_name = list(psutil.net_if_stats().keys())[1]
+    nic_speed = psutil.net_if_stats()[if_name][2]  # get(if_name).speed
+    if nic_speed == 0:
+        nic_speed = 100
+    r_byte_1 = psutil.net_io_counters(pernic=True).get(if_name).bytes_recv
+    s_byte_1 = psutil.net_io_counters(pernic=True).get(if_name).bytes_sent
+    time.sleep(time_quanta)
+    r_byte_delta = int(psutil.net_io_counters(pernic=True).get(if_name).bytes_recv) - int(r_byte_1)
+    s_byte_delta = int(psutil.net_io_counters(pernic=True).get(if_name).bytes_sent) - int(s_byte_1)
+    byte_per_sec = (r_byte_delta + s_byte_delta) / time_quanta
+    per_util_nic = (byte_per_sec * 100 / (nic_speed * 2 ** 17))
+    print(byte_per_sec)
+    print(per_util_nic)
+
+
 def get_resource_util():
     h1 = Thread(target=get_mem)
     h2 = Thread(target=get_cpu)
@@ -67,6 +94,7 @@ def get_resource_util():
     h1.start()
     h2.start()
     h3.start()
+
 
 def main():
     global v_store
@@ -80,7 +108,6 @@ def main():
     try:
         while True:
             get_resource_util()
-            drawnow(plot_resource_util)
             time.sleep(2)
     except KeyboardInterrupt:
         print('Programme Terminated')
