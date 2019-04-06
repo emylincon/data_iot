329d3f3fedd7e71d5a316c0be073087d0b3693d0
Emeka Ugwuanyi <emylincon@gmail.com>
2019-04-06 19:15:46 +0100

    updated file analysis.py, removed redundancy
    Signed-off-by: Emeka Ugwuanyi <emylincon@gmail.com>

--- data/analysis.py
+++ data/analysis.py
@@ -23,12 +23,6 @@ mem = [float(i[0:-1]) for i in dic['mem'][1:-1]]
 cpu = [float(i[0:-1]) for i in dic['cpu'][1:-1]]
 store = [float(i[0:-1]) for i in dic['store'][1:-1]]
 net = [float(i[0:-1]) for i in dic['net'][1:-1]]
-'''
-print('mem: ', mem)
-print('cpu: ', cpu)
-print('store: ', store)
-print('net: ', net)
-'''
 
 
 def average(var):
